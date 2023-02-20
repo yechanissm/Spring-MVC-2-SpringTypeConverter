8f3704414eb7940e4c47503604906c137b705fcb
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-02-20 19:29:03 +0900

    Integer - String 컨버터

---
+++ src/test/java/hello/typeconverter/converter/ConverterTest.java
@@ -0,0 +1,23 @@
+package hello.typeconverter.converter;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ConverterTest {
+
+    @Test
+    void stringToInteger(){
+        StringToIntegerConverter converter = new StringToIntegerConverter();
+        Integer result = converter.convert("10");
+        assertThat(result).isEqualTo(10);
+    }
+
+    @Test
+    void IntegerToString(){
+        IntegerToStringConverter converter = new IntegerToStringConverter();
+        String result = converter.convert(10);
+        assertThat(result).isEqualTo("10");
+    }
+}
