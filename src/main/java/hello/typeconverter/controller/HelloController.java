71e603e9f3cc4dde435772c8394b8158af9266ec
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-02-20 20:48:53 +0900

    스프링에 Converter 적용하기

--- src/main/java/hello/typeconverter/controller/HelloController.java
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -1,6 +1,7 @@
 package hello.typeconverter.controller;
 
 
+import hello.typeconverter.type.IpPort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,8 +20,15 @@ public class HelloController {
     }
     
     @GetMapping("/hello-v2")
-    public String helloV(@RequestParam Integer data){
+    public String helloV2(@RequestParam Integer data){
         System.out.println("data = " + data);
+        return "ok";
+    }
+
+    @GetMapping("/ip-port")
+    public String ipPort(@RequestParam IpPort ipPort){
+        System.out.println("ipPort.getIp() = " + ipPort.getIp());
+        System.out.println("ipPort.getPort() = " + ipPort.getPort());
         return "ok";
     }
 }
