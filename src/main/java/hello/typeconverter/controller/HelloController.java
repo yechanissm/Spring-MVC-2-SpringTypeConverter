7043e84b7e4e66c5c85fb5d97f3cebed1bd2df7a
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-02-20 19:02:22 +0900

    스프링 타입 컨버터 소개

--- src/main/java/hello/typeconverter/controller/HelloController.java
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -1,8 +1,26 @@
 package hello.typeconverter.controller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class HelloController {
+
+    @GetMapping("/hello-v1")
+    public String helloV1(HttpServletRequest request) {
+        String data = request.getParameter("data"); //문자 타입조회
+        Integer intvalue = Integer.valueOf(data);
+        System.out.println("intvalue = " + intvalue);
+        return "ok";
+    }
+    
+    @GetMapping("/hello-v2")
+    public String helloV(@RequestParam Integer data){
+        System.out.println("data = " + data);
+        return "ok";
+    }
 }
